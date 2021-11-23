aa564a6fcfd54c80127b5833e17dbe3e50ed4f17
Elliott Clarke <ecclarke42@gmail.com>
2021-11-23 14:53:20 -0500

    Day 3, Part 2

--- examples/day03.rs
+++ examples/day03.rs
@@ -9,9 +9,27 @@ fn main() {
     let field = Field::new(INPUT);
 
     println!("Part 1:");
-    let trees = field.traverse_cycle(1, 3);
+    let path_1_3 = field.traverse_cycle(1, 3);
     // let trees = field.traverse_collect(1, 3);
-    println!("\tFound\t{} trees", trees);
+    println!("\tFound\t{} trees", path_1_3);
+
+    println!("Part 2:");
+    let path_1_1 = field.traverse_cycle(1, 1);
+    // let path_1_3 = field.traverse_cycle(1, 1);
+    let path_1_5 = field.traverse_cycle(1, 5);
+    let path_1_7 = field.traverse_cycle(1, 7);
+    let path_2_1 = field.traverse_cycle(2, 1);
+    // let run_1_1 = field.traverse_cycle(1, 1);
+    println!("\tRight 1, Down 1: \t{} trees", path_1_1);
+    println!("\tRight 3, Down 1: \t{} trees", path_1_3);
+    println!("\tRight 5, Down 1: \t{} trees", path_1_5);
+    println!("\tRight 7, Down 1: \t{} trees", path_1_7);
+    println!("\tRight 1, Down 2: \t{} trees", path_2_1);
+    println!();
+    println!(
+        "\tProduct = {}",
+        path_1_1 * path_1_3 * path_1_5 * path_1_7 * path_2_1
+    )
 }
 
 #[cfg(test)]
@@ -20,7 +38,18 @@ mod tests {
 
     #[test]
     fn part_1() {
-        assert_eq!(Field::new(INPUT).traverse_cycle(1, 3), 148);
-        assert_eq!(Field::new(INPUT).traverse_collect(1, 3), 148)
+        let field = Field::new(INPUT);
+        assert_eq!(field.traverse_cycle(1, 3), 148);
+        assert_eq!(field.traverse_collect(1, 3), 148)
+    }
+
+    #[test]
+    fn part_2() {
+        let field = Field::new(INPUT);
+        assert_eq!(field.traverse_cycle(1, 1), 50);
+        assert_eq!(field.traverse_cycle(1, 3), 148);
+        assert_eq!(field.traverse_cycle(1, 5), 53);
+        assert_eq!(field.traverse_cycle(1, 7), 64);
+        assert_eq!(field.traverse_cycle(2, 1), 29);
     }
 }
